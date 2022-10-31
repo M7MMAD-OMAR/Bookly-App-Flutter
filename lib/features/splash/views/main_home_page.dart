8c60529adb9eb612e099e02f046767dd5bc68688
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-10-31 16:43:18 +0300

    change app name and edit some files then create home page

---
+++ lib/features/splash/views/main_home_page.dart
@@ -0,0 +1,17 @@
+
+import 'package:first_project/features/splash/views/widget/home_page_body.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return HomePageBody();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'features/splash/views/main_home_page.dart';
+
 void main() {
-  runApp(const MovieApp());
+  runApp(const BookApp());
 }
 
-class MovieApp extends StatelessWidget {
-  const MovieApp({Key? key}) : super(key: key);
+class BookApp extends StatelessWidget {
+  const BookApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: MainHomePage(),
+    return const GetMaterialApp(
+      home: HomePage(),
     );
   }
 }
